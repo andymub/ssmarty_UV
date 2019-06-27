10adcedcd3e7c53cdf821097ae5b85563149786d
Andymub <Supinf0>
2019-06-27 15:21:57 +0100

    activ_student

--- app/src/main/java/ssmarty/univ/MainActivity.java
+++ app/src/main/java/ssmarty/univ/MainActivity.java
@@ -2,40 +2,67 @@ package ssmarty.univ;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcManager;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG ="NFC" ;
     private ImageView imgNfcState;
     private TextView txtStateNfc;
+    public  AnimationDrawable frameAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Context context=getApplicationContext();
         imgNfcState= findViewById(R.id.imgNfcState);
+
+       // imgNfcState.setBackground(ResourcesCompat.getDrawable(getResources(), R.mipmap.ssmart_nfc_connect, null));
         txtStateNfc=findViewById(R.id.txtStateNfc);
         ncfCompatible ();
+
+        imgNfcState.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent studentIntent;
+                studentIntent=new Intent(MainActivity.this, MainActivity_student.class);
+                startActivity(studentIntent);
+            }
+        });
+        imgNfcState.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return false;
+            }
+        });
+
 
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        ncfCompatible ();
+       // ncfCompatible ();
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         ncfCompatible ();
+
     }
 
     public void ncfCompatible (){
@@ -48,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         else{
             if (nfcAdapter.isNdefPushEnabled()){
                 Toast.makeText(getApplicationContext(), R.string.ntc_activE,Toast.LENGTH_LONG).show();
-                imgNfcState.setImageResource(R.mipmap.ssmart_nfc_deconnect);
+                imgNfcState.setImageResource(R.mipmap.nfc_ouvert_round);
                 txtStateNfc.setText(R.string.ntc_activE);
 
 
@@ -56,6 +83,8 @@ public class MainActivity extends AppCompatActivity {
             else if (nfcAdapter.isEnabled()){
                 Toast.makeText(getApplicationContext(),"ouvert",Toast.LENGTH_LONG).show();
                // imgNfcState.setImageResource(R.mipmap.ssmart_nfc_connect);
+                // Start the animation (looped playback by default).
+                frameAnimation.start();
             }
             else {
                 Toast.makeText(getApplicationContext(), R.string.veuill_activE_Nfc,Toast.LENGTH_LONG).show();
@@ -65,4 +94,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
 }
